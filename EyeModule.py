3efba68e140c4a02bfaa85c6b1b5821821fdb305
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-08-17 12:07:40 -0300

    coloquei o detector de hist para a pupila

--- EyeModule.py
+++ EyeModule.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.core.numeric import NaN
 class iris_detection():
     def __init__(self, image=None, data_save = None):
         '''
@@ -19,90 +20,18 @@ class iris_detection():
         ret, o4 = cv2.threshold(img,0,255, cv2.THRESH_TOZERO_INV + cv2.THRESH_OTSU )
         ret, o5 = cv2.threshold(img,0,255, cv2.THRESH_TRUNC + cv2.THRESH_OTSU )
         return [o1,o2,o3,o4,o5]
-    def detect_eye_features(self):
-        
-        gray = self.convert_to_gray_scale(self._img)
-
-        
-        o1,o2,o3,o4,o5 = self.apply_otsus(gray)
-        #cv2.imshow("OTSU 1", o1)
-        #cv2.imshow("OTSU 2", o2)
-        #cv2.imshow("OTSU 3", o3)
-        #cv2.imshow("OTSU 4", o4)
-        #cv2.imshow("OTSU 5", o5)
-        blured = cv2.bilateralFilter(o5,10,50,50)
-        #cv2.imshow("bin_img", bin_img)
-
-
-        #print(bin_img.shape)
-        minDist = 1
-        param1 = 25 # 500
-        param2 = 20 # 200 #smaller value-> more false circles
-        minRadius = 1
-        maxRadius = 300 #10
-
-        # docstring of HoughCircles: HoughCircles(image, method, dp, minDist[, circles[, param1[, param2[, minRadius[, maxRadius]]]]]) -> circles
-        circles = cv2.HoughCircles(blured, cv2.HOUGH_GRADIENT, 1, minDist, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
-        x,y,r = [0],[0],[[0,0]]
-        if circles is not None:
-            circles = np.uint16(np.around(circles))
-            i = circles[0][0]
-            x,y,r = [i[0]], [i[1]], [[i[2],i[2]]]
-            if len(circles[0])>1:
-                #print(circles[0],len(circles[0]))
-                #print(circles[0][0])
-                if circles[0][1].all is not circles[0][0]:
-                    j = circles[0][1]
-                    x.append(j[0])
-                    y.append(j[1])
-                    r.append([j[2],j[2]])
-            #print(i[0])
-            #print(i)
-        return x,y,r
     
-    def detect_iris_hist(self):
-        gray = self.convert_to_gray_scale(self._img)
-
-        
-        o1,o2,o3,o4,o5 = self.apply_otsus(gray)
-        blurred = cv2.bilateralFilter(o5,20,40,50)
-        o1,o2,o3,o4,o5 = self.apply_otsus(blurred)
-        cv2.imshow("OTSU 1", o1)
-        cv2.imshow("OTSU 2", o2)
-        cv2.imshow("OTSU 3", o3)
-        cv2.imshow("OTSU 4", o4)
-        cv2.imshow("OTSU 5", o5)
-        cv2.imshow("blur",blurred)
-        #ret, bin_img = cv2.threshold(blurred,127,255,cv2.THRESH_BINARY)
-        ret, bin_img = cv2.threshold(blurred,int((2*blurred.mean())/2),255,cv2.THRESH_BINARY)
-
-
-        kernel = np.ones((5,5),np.uint8)
-        cv2.imshow("bin_iris",bin_img)
-        
-        '''bin_img = cv2.erode(bin_img,kernel,iterations = 3)
-        cv2.imshow("Erosion",bin_img)'''
-        bin_img = cv2.morphologyEx(bin_img, cv2.MORPH_OPEN, kernel)
-        cv2.imshow("MORPH_OPEN IRIS",bin_img)
-        bin_img = cv2.bilateralFilter(bin_img,50,100,50)
-        bin_img = cv2.bilateralFilter(bin_img,50,100,50)
-        bin_img = cv2.morphologyEx(bin_img, cv2.MORPH_CLOSE, kernel)
-        
-        ret, bin_img = cv2.threshold(bin_img,bin_img.min()+40,255,cv2.THRESH_BINARY)
-        cv2.imshow("MORPH_CLOSE IRIS",bin_img)
-        
-        
-        cv2.waitKey(1)
-        col_count = np.zeros((bin_img.shape[1]))
+    def hist_analisys(self,img):
+        col_count = np.zeros((img.shape[1]))
         row_count = []
-        for i in range(bin_img.shape[0]):
+        for i in range(img.shape[0]):
             aux_row = 0
             aux_col = 0
-            for j in range(bin_img.shape[1]):
-                aux_row = aux_row + bin_img[i][j]/bin_img.shape[1]
-                col_count[j] = col_count[j] + bin_img[i][j]/bin_img.shape[0]
+            for j in range(img.shape[1]):
+                aux_row = aux_row + img[i][j]/img.shape[1]
+                col_count[j] = col_count[j] + img[i][j]/img.shape[0]
             row_count.append(aux_row)
-        #cv2.imshow("bin_img", bin_img)
+        #cv2.imshow("img", img)
         for i in range(len(row_count)):
             row_count[i] =( 255 -row_count[i])
         for i in range(len(col_count)):
@@ -135,14 +64,54 @@ class iris_detection():
                 #if col>0:
                     desvio_c = media_c - i
                     break
-        if media_c is None or media_c == [] :
+        
+        if media_c is None or media_c == [] or media_c == NaN :
             media_c = 0
-        if media_r is None or media_r == [] :
+        if media_r is None or media_r == [] or media_r == NaN:
             media_r = 0
         if desvio_r is None or desvio_r == [] or desvio_r<0 :
             desvio_r = 0
         if desvio_c is None or desvio_c == [] or desvio_c<0 :
             desvio_c = 0
+        return row_count,col_count,media_c,media_r,desvio_c,desvio_r
+
+    def detect_iris_hist(self):
+        gray = self.convert_to_gray_scale(self._img)
+
+        
+        o1,o2,o3,o4,o5 = self.apply_otsus(gray)
+        blurred = cv2.bilateralFilter(o5,20,40,50)
+        o1,o2,o3,o4,o5 = self.apply_otsus(blurred)
+        '''cv2.imshow("OTSU 1", o1)
+        cv2.imshow("OTSU 2", o2)
+        cv2.imshow("OTSU 3", o3)
+        cv2.imshow("OTSU 4", o4)
+        cv2.imshow("OTSU 5", o5)'''
+        cv2.imshow("blur",blurred)
+        #ret, bin_img = cv2.threshold(blurred,127,255,cv2.THRESH_BINARY)
+        ret, bin_img = cv2.threshold(blurred,int((2*blurred.mean())/2),255,cv2.THRESH_BINARY)
+
+
+        kernel = np.ones((5,5),np.uint8)
+        cv2.imshow("bin_iris",bin_img)
+        
+        '''bin_img = cv2.erode(bin_img,kernel,iterations = 3)
+        cv2.imshow("Erosion",bin_img)'''
+        bin_img = cv2.morphologyEx(bin_img, cv2.MORPH_OPEN, kernel)
+        cv2.imshow("MORPH_OPEN IRIS",bin_img)
+        bin_img = cv2.bilateralFilter(bin_img,50,100,50)
+        bin_img = cv2.bilateralFilter(bin_img,50,100,50)
+        bin_img = cv2.morphologyEx(bin_img, cv2.MORPH_CLOSE, kernel)
+        
+        ret, bin_img = cv2.threshold(bin_img,bin_img.min()+40,255,cv2.THRESH_BINARY)
+        cv2.imshow("MORPH_CLOSE IRIS",bin_img)
+        
+        
+        cv2.waitKey(1)
+
+
+        row_count,col_count,media_c,media_r,desvio_c,desvio_r = self.hist_analisys(bin_img)
+        
         if self._data != None:
             if type(self._data["left_eye"]["blurred"]) == np.ndarray:
                 self._data["right_eye"]["blurred"] = blurred
@@ -196,25 +165,15 @@ class iris_detection():
         cv2.imshow("EROSION",new_img)
         
         
-        contours,hierarchy = cv2.findContours(new_img, 1, 2)
-        for cnt in contours:
-            (x,y),radius = cv2.minEnclosingCircle(cnt)
-            center = [int(x),int(y)]
-            radius = int(radius)
-            cv2.circle(self._img,center,radius,(0,255,255),2)
-            cv2.imshow("dd",self._img)
-            cv2.waitKey(2)
-        if(len(contours) == 0):
-            return (0,0),0
+        row_count,col_count,media_c,media_r,desvio_c,desvio_r = self.hist_analisys(new_img)
         
-        if len(contours)>1:
-            cnt = contours[1]
-        else:
-            cnt = contours[0]
-        (x,y),radius = cv2.minEnclosingCircle(cnt)
-        center = [int(x),int(y)]
-        radius = int(radius)
-        return center,radius
+        try:
+            
+            center = [int(media_c), int(media_r)]
+            radius = int(desvio_c)
+            return center,radius
+        except:
+            return [0,0],0
     def start_detection(self):
         if self._img is not None:
             #return self.detect_eye_features()
